9255d13798400a06622e9564588ac3c39ce48e4d
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-04-28 20:27:08 -0300

    EP de cripto, BEGIN.

---
+++ eps/mac0336-cripto/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    puts("PUTS PUTS PUTS PUTS");
+    return 0;
+}
---
+++ eps/mac0336-cripto/types.h
@@ -0,0 +1,7 @@
+#ifndef TYPES_H_
+#define TYPES_H_
+
+typedef unsigned char byte;
+
+
+#endif /* TYPES_H_ */
