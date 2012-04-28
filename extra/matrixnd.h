dac6a518cf5e5701e732e05136f3ee1c151b93d6
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2012-04-28 20:25:27 -0300

    Matrix NxM, not only NxN

--- extra/matrixnd.h
+++ extra/matrixnd.h
@@ -7,27 +7,25 @@
 #include <stdarg.h>
 #include "vectornd.h"
 
-/* Always NxN! */
-
-template<int N>
+template<int N, int M = N>
 class Matrix {
-    Vector<N> val[N];
+    Vector<M> val[N];
 public:
 	Matrix() {}
-    Matrix(Vector<N> a, ...) {
+    explicit Matrix(Vector<M> a, ...) {
         val[0] = a;
         va_list vl;
         va_start(vl, a);
         for(int i = 1; i < N; ++i) {
-            val[i] = va_arg(vl, Vector<N>);
+            val[i] = va_arg(vl, Vector<M>);
         }
         va_end(vl);    
     }
 	~Matrix() {}
     
-    Matrix<N> Transpose() const {
-        Matrix<N> result;
-        for(int i = 0; i < N; ++i)
+    Matrix<M, N> Transpose() const {
+        Matrix<M, N> result;
+        for(int i = 0; i < M; ++i)
             for(int j = 0; j < N; ++j)
                 result[i][j] = val[j][i];
         return result;
@@ -53,6 +51,7 @@ public:
         return a;
     }
     
+    // TODO: make this MxN?
 	bool QR(Matrix<N> &q, Matrix<N> &r) const {
         // Cria uma cópia da matrix atual
         Matrix<N> v = *this;
@@ -72,71 +71,71 @@ public:
     }
 
 	double operator() (int i, int j)    const { return val[i][j]; }
-          Vector<N>& operator [](int i)       { return val[i]; }
-    const Vector<N>& operator [](int i) const { return val[i]; }
+          Vector<M>& operator [](int i)       { return val[i]; }
+    const Vector<M>& operator [](int i) const { return val[i]; }
     
-	friend std::ostream& operator<< (std::ostream& io, const Matrix<N>& ma) {
-        Matrix<N> m = ma.Transpose();
-        for(int i = 0; i < N; ++i) {
+	friend std::ostream& operator<< (std::ostream& io, const Matrix<N, M>& ma) {
+        Matrix<M, N> m = ma.Transpose();
+        for(int i = 0; i < M; ++i) {
             if(i > 0) io << "\n";
             io << m[i];
         }
         return io;
     }
     
-    Matrix<N> operator+=(const Matrix<N>& rhs) {
+    Matrix<N, M> operator+=(const Matrix<N, M>& rhs) {
         for(int i = 0; i < N; ++i)
             val[i] += rhs[i];
         return *this;
     }
-    Matrix<N> operator+(const Matrix<N>& rhs) const {
-        Matrix<N> result = *this;
+    Matrix<N, M> operator+(const Matrix<N, M>& rhs) const {
+        Matrix<N, M> result = *this;
         result += rhs;
         return result;
     }
-    Matrix<N> operator-() const {
-        Matrix<N> result = *this;
+    Matrix<N, M> operator-() const {
+        Matrix<N, M> result = *this;
         for(int i = 0; i < N; ++i)
             result[i] = -result[i];
         return result;
     }
-    Matrix<N> operator-=(const Matrix<N>& rhs) {
+    Matrix<N, M> operator-=(const Matrix<N, M>& rhs) {
         for(int i = 0; i < N; ++i)
             val[i] -= rhs[i];
         return *this;
     }
-    Matrix<N> operator-(const Matrix<N>& rhs) const {
-        Matrix<N> result = *this;
+    Matrix<N, M> operator-(const Matrix<N, M>& rhs) const {
+        Matrix<N, M> result = *this;
         result -= rhs;
         return result;
     }
-    Matrix<N> operator*=(const double& scalar) {
+    Matrix<N, M> operator*=(const double& scalar) {
         for(int i = 0; i < N; ++i)
             val[i] *= scalar;
         return *this;
     }
-    Matrix<N> operator*(const double& scalar) const {
-        Matrix<N> result = *this;
+    Matrix<N, M> operator*(const double& scalar) const {
+        Matrix<N, M> result = *this;
         result *= scalar;
         return *this;
     }
     
-    Vector<N> operator*(const Vector<N>& v) const {
-        Vector<N> result;
+    Vector<M> operator*(const Vector<M>& v) const {
+        Vector<M> result;
         for(int i = 0; i < N; ++i)
             result += val[i] * v[i];
         return result;
     }
     
-    Matrix<N> operator*(const Matrix<N>& rhs) const {
-        Matrix<N> result;
+    Matrix<N, N> operator*(const Matrix<M, N>& rhs) const {
+        Matrix<N, N> result;
         for(int i = 0; i < N; ++i)
             result[i] = (*this) * rhs[i];
     }
 };
 
-template<int N>
-Matrix<N> operator*(const double& scalar, const Matrix<N>& r) {
+template<int N, int M>
+Matrix<N, M> operator*(const double& scalar, const Matrix<N, M>& r) {
 	return r * scalar;
 }
 
--- extra/vectornd-test.cpp
+++ extra/vectornd-test.cpp
@@ -4,18 +4,26 @@
 int main(int argc, char** argv) {
     Vector<2> a(5.0, 4.0);
     Vector<3> b1(1.0, 0.0, 0.0), b2(0.0, 1.0, 0.0), b3(0.0, 0.0, 1.0);
-    Vector<4> c(a);
+    Vector<4> c1(0.0, 0.0, 0.0, 0.0), c2(0.0, 0.0, 0.0, 0.0);
     
-    Matrix<3> m(b1, b2, b3);
-    m *= 2;
+    Matrix<3> m1(b1, b2, b3);
+    m1 *= 2;
+    
+    Matrix<2, 4> m2(c1, c2);
+    Matrix<4, 2> m2t = m2.Transpose();
     
     std::cout << a << "; Norm: " << a.Norm() << std::endl;
-    std::cout << c << std::endl;
-    std::cout << std::endl << "m:\n" << m << std::endl << std::endl;
+    std::cout << c1 << std::endl;
+    std::cout << std::endl << "m1:\n" << m1 << std::endl << std::endl;
+    std::cout << std::endl << "m2:\n" << m2 << std::endl << std::endl;
+    std::cout << std::endl << "m2t:\n" << m2t << std::endl << std::endl;
     
     Vector<3> r = b1 + 2*b2 + b3;
-    std::cout << "    r = " << r << std::endl;
-    r = m * r;
-    std::cout << "m * r = " << r << std::endl;
+    std::cout << "     r = " << r << std::endl;
+    r = m1 * r;
+    std::cout << "m1 * r = " << r << std::endl;
+    
+    Matrix<2, 2> p = m2 * m2t;
+    std::cout << "\nm1 * m2 =\n" << p << std::endl;
     return 0;
 }
